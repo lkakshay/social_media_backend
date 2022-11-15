4be0bf1e97cc6bc0c2e2638e5fdb42cb4674f688
lkakshay <connectme.lkakshay@gmail.com>
2022-11-15 18:13:17 +0530

    home api completed

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -14,21 +14,33 @@ const create = async (req, res) => {
 };
 
 const byuser = async (req, res) => {
+  const skip = (+req.params.page - 1) * 10;
   try {
     const client = await User.findOne({ _id: req.body.user_id });
-    const user = await User.findOne({ username: req.params.username }).lean();
-    const NoOfDocs = await Post.find({ user_id: user._id }).count();
-    const totalpages = Math.ceil(NoOfDocs / 10);
-    const data = await Post.aggregate([
+   
+    const user = await User.findOne({ username: req.params.username });
+    const docs = await User.aggregate([
       {
         $match: {
-          user_id: user._id,
+          _id: user._id,
         },
       },
 
       {
+        $project: { _id: 0, user_id: "$_id", username: 1 },
+      },
+      {
+        $lookup: {
+          from: "posts",
+          localField: "user_id",
+          foreignField: "user_id",
+          as: "post",
+        },
+      },
+      { $unwind: "$post" },
+      {
         $sort: {
-          createdAt: -1,
+          "post.createdAt": -1,
         },
       },
       {
@@ -39,59 +51,64 @@ const byuser = async (req, res) => {
           as: "bio",
         },
       },
+      { $unwind: "$bio" },
       {
-        $lookup: {
-          from: "users",
-          localField: "user_id",
-          foreignField: "_id",
-          as: "user",
+        $project: {
+          post: 1,
+          user_id: 1,
+          profilePic: "$bio.imgUrl",
         },
       },
       {
         $lookup: {
           from: "likes",
-          localField: "_id",
+          localField: "post._id",
           foreignField: "post_id",
-          as: "a",
+          as: "like",
         },
       },
       {
         $lookup: {
           from: "users",
-          localField: "a.user_id",
+          localField: "like.user_id",
           foreignField: "_id",
-          as: "liked",
+          as: "likedUsers",
         },
       },
-
-      { $unwind: "$bio" },
-
-      { $unwind: "$user" },
-
-      { $skip: 10 * (req.params.page - 1) },
-      { $limit: 10 },
-
       {
         $project: {
-          _id: 1,
-          profilePic: "$bio.imgUrl",
-          username: "$user.username",
-          content: 1,
-          imgUrl: 1,
-          createdAt: 1,
-          updatedAt: 1,
-          liked: {
-            _id: 1,
-            username: 1,
-          },
-          isLiked: {
-            $in: [client._id, "$a.user_id"],
-          },
+          user_id: 1,
+          profilePic: 1,
+          username: 1,
+          post: 1,
+          "likedUsers.username": 1,
+          "likedUsers._id": 1,
+          username: 1,
+          isLiked: { $in: [client._id, "$likedUsers._id"] },
+        },
+      },
+      {
+        $project: {
+          "post.user_id": 0,
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          ROOT: { $push: "$$ROOT" },
+          total: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          totalPages: { $ceil: { $divide: ["$total", 10] } },
+          posts: { $slice: ["$ROOT", skip, 10] },
         },
       },
     ]);
 
-    return res.status(200).send({ posts: data, totalpages });
+    return res.status(200).send(docs[0]);
   } catch (e) {
     console.log("e", e);
     res.status(500).send(e.message);
@@ -135,15 +152,37 @@ const explore = async (req, res) => {
       {
         $sample: { size: 20 },
       },
+      {
+        $group: {
+          _id: null,
+          post: { $push: "$$ROOT" },
+        },
+      },
 
+      {
+        $project: {
+          _id: 0,
+          post: 1,
+        },
+      },
+      { $unwind: "$post" },
       {
         $lookup: {
           from: "bios",
-          localField: "user_id",
+          localField: "post.user_id",
           foreignField: "user_id",
           as: "bio",
         },
       },
+      { $unwind: "$bio" },
+      {
+        $project: {
+          post: 1,
+          user_id: "$bio.user_id",
+          profilePic: "$bio.imgUrl",
+        },
+      },
+
       {
         $lookup: {
           from: "users",
@@ -152,43 +191,43 @@ const explore = async (req, res) => {
           as: "user",
         },
       },
+      { $unwind: "$user" },
+      {
+        $project: {
+          post: 1,
+          user_id: 1,
+          profilePic: 1,
+          username: "$user.username",
+        },
+      },
       {
         $lookup: {
           from: "likes",
-          localField: "_id",
+          localField: "post._id",
           foreignField: "post_id",
-          as: "a",
+          as: "like",
         },
       },
       {
         $lookup: {
           from: "users",
-          localField: "a.user_id",
+          localField: "like.user_id",
           foreignField: "_id",
-          as: "liked",
+          as: "likedUsers",
         },
       },
-
-      { $unwind: "$bio" },
-
-      { $unwind: "$user" },
 
       {
         $project: {
           _id: 1,
-          profilePic: "$bio.imgUrl",
+          user_id: 1,
+          profilePic: 1,
           username: "$user.username",
-          content: 1,
-          imgUrl: 1,
-          createdAt: 1,
-          updatedAt: 1,
-          liked: {
-            _id: 1,
-            username: 1,
-          },
-          isLiked: {
-            $in: [client._id, "$a.user_id"],
-          },
+          post: 1,
+          "likedUsers.username": 1,
+          "likedUsers._id": 1,
+          username: 1,
+          isLiked: { $in: [client._id, "$likedUsers._id"] },
         },
       },
     ]);
@@ -305,7 +344,6 @@ const home = async (req, res) => {
           _id: 1,
           user_id: 1,
           profilePic: 1,
-          username: "$user.username",
           post: 1,
           "likedUsers.username": 1,
           "likedUsers._id": 1,
@@ -316,29 +354,24 @@ const home = async (req, res) => {
       {
         $project: {
           _id: 0,
-          "post.user_id":0
-          
+          "post.user_id": 0,
         },
       },
-
       {
         $group: {
-          _id:null,
+          _id: null,
           ROOT: { $push: "$$ROOT" },
-          totalpages: { $sum: 1 },
+          total: { $sum: 1 },
         },
       },
       {
         $project: {
-          _id:0,
-          totalpages: 1,
+          _id: 0,
+          totalPages: { $ceil: { $divide: ["$total", 10] } },
           posts: { $slice: ["$ROOT", skip, 10] },
-
         },
       },
     ]);
-
-    console.log("docs", docs[0]);
     return res.status(200).send(docs[0]);
   } catch (e) {
     console.log("e", e);
